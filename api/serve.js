16a9a12861af287a5fd274d768b666b84f18eed3
symbiorithmicOntologist <zakdegarmo@gmail.com>
2025-08-27 13:27:28 -0400

    more fafo

--- api/serve.js
+++ api/serve.js
@@ -1,18 +1,10 @@
+import { readFileSync } from 'fs';
+import { join } from 'path';
+
 export default async function handler(req) {
-  const fs = require('fs');
-  const path = require('path');
-
-  const filePath = path.join(process.cwd(), '3D-FileExplorer.html');
-
-  try {
-    const html = fs.readFileSync(filePath, 'utf8');
-    return new Response(html, {
-      status: 200,
-      headers: { 'Content-Type': 'text/html' },
-    });
-  } catch (err) {
-    return new Response(`Error loading HTML: ${err.message}`, {
-      status: 500,
-    });
-  }
+  const html = readFileSync(join(__dirname, '3D-FileExplorer.html'), 'utf8');
+  return new Response(html, {
+    status: 200,
+    headers: { 'Content-Type': 'text/html' },
+  });
 }
